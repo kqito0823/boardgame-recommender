6226b251e3309d027965b49ad6f63a33170f8921
kqito0823 <kit.6104@icloud.com>
2026-03-05 15:47:17 +0900

    Co-authored-by: takahiro-hirano <takahiro-hirano67@users.noreply.github.com>

---
+++ next-app/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // App Routerの場合
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // Pages Routerの場合
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // srcフォルダを使用している場合
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
