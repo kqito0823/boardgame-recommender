80bb17e412a74ffae87f10fbc878298f3f588b42
kqito0823 <kit.6104@icloud.com>
2026-03-27 18:36:39 +0900

    Update page.tsx

--- next-app/app/board-games/page.tsx
+++ next-app/app/board-games/page.tsx
@@ -1,7 +1,7 @@
 import BoardGamesClientPage from "./ClientPage";
 import { Game } from "@/types/game";
 
-const url = process.env.NEXT_PUBLIC_API_URL!;
+const url = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000";
 
 export default async function BoardGamesPage() {
     const response = await fetch(`${url}/api/db/get_game_table`);
